bad29836c6261c3c74e96d3a6452bbe96ac367cf
AlexandrKruglov <krulalex84@gmail.com>
2024-01-21 06:08:46 +0300

    new function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,10 @@
 def transform_leters_upp(text):
+    """переводит все буквы в верхний рег"""
     new_text = text.upper()
+    return new_text
+
+
+def transform_first_letter_upp(text):
+    """переводит первую букву каждого слова в верхний рег"""
+    new_text = text.title()
     return new_text
